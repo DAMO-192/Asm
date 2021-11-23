d121c5f3c9e63e457580b19e1e2bdb1233d36c5a
yang <1925077599@qq.com>
2021-11-23 16:07:25 +0800

    litemi 0.0.1
    Signed-off-by: yang <1925077599@qq.com>

--- main.go
+++ main.go
@@ -1,27 +1,11 @@
 package main
 
 import (
-	"Asm/conn"
+	"Asm/api"
 	"Asm/databases"
-	"Asm/middleware"
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	databases.InitDB()
-
-	e := gin.Default()
-	e.Use(middleware.Cor())
-	userV1 := e.Group("/api/user")
-	userV1.POST("/register", conn.Register)
-	userV1.POST("/login", conn.Login)
-	userV1.GET("/Info", middleware.AuthMiddleware(), conn.Info)
-	userV1.GET("/menulist", middleware.AuthMiddleware(), conn.MenuList)
-	working := e.Group("/api/working")
-	working.POST("/register", middleware.AuthMiddleware(), conn.AccsessReginst)
-	working.GET("/info", middleware.AuthMiddleware(), conn.Accsessinfo)
-	working.POST("/Personnel", middleware.AuthMiddleware(), conn.PersonnelRegistration)
-	working.POST("/getPermission", middleware.AuthMiddleware(), conn.GetPermission)
-	working.POST("/menulist", middleware.AuthMiddleware(), conn.Menulist)
-	e.Run(":80")
+	api.StartService()
 }
